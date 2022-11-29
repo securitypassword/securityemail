484927b14b1bf4114e5bb40f06dd6ff4e69d7efb
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-28 18:38:58 -0600

    email

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const cors = require("cors")
+const bodyParser = require("body-parser")
 const app = express()
 const PORT = process.env.PORT;
+const email = require("./js/email.js")
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+//global
+//que si no da error
+const corsConfig = {
+    credentials: true,
+    origin: true,
+};
+app.use(cors(corsConfig));
+//end of global
+
+
+await email.runEmail(app)
+
+app.get('/', async function (req, res) {
+  res.send('security emails')
 })
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
-  });  
+  });
---
+++ js/email.js
@@ -0,0 +1,27 @@
+var XMLHttpRequest = require('xhr2');
+var xhr = new XMLHttpRequest();
+var emailjs = '@emailjs/browser'
+
+const sendEmail = async function(to, subject, content){
+    console.log("send email",to,subject,content)
+    await emailjs.send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, {
+        subject:subject, content:content, to:to})
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+}
+
+const runEmail = async function(app){
+    emailjs.init(process.env.EMAIL_PUBLIC_KEY)
+    app.get("/test", async (req, res, next) => {
+        await sendEmail("martin.sainos.demian@gmail.com","test",req.query.text)
+        res.json({
+            msg:"email"
+        });
+    });
+}
+
+module.exports.runEmail = runEmail;
+module.exports.sendEmail = sendEmail;
